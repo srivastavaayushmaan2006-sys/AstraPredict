d0724c3062195b939a4e5a7d9d0091160e45c76c
Ayushmaan Srivastava <srivastavaayushmaan2006@gmail.com>
2026-07-30 07:58:48 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,7 @@
+from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
+
+SPACEX_API = "https://api.spacexdata.com/v5/launches"
