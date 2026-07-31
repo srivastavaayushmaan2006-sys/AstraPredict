88cd1e206a8aedbdef5e75328dadb377e7fa7ba6
Ayushmaan Srivastava <srivastavaayushmaan2006@gmail.com>
2026-07-31 00:28:43 -0500

    Update api_clients.py

--- src/api_clients.py
+++ src/api_clients.py
@@ -1,1 +1,17 @@
+import requests
 
+from src.config import API_ENDPOINTS
+
+
+def get_launch_library_data():
+    """
+    Download launch data from Launch Library 2.
+    """
+
+    url = API_ENDPOINTS["launch_library"]
+
+    response = requests.get(url, timeout=30)
+
+    response.raise_for_status()
+
+    return response.json()
