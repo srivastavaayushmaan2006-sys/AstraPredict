99457b943664b5035e0f43d9ce72795f60facfd9
Ayushmaan Srivastava <srivastavaayushmaan2006@gmail.com>
2026-07-31 00:29:28 -0500

    Update data_ingestion.py

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,32 +1,25 @@
 import json
-import requests
 
-from config import RAW_DATA_DIR, SPACEX_API
+from src.api_clients import get_launch_library_data
+from src.config import RAW_DATA_DIR
 
 
-def fetch_launches():
-    """
-    Download all SpaceX launches.
-    """
+def save_raw_launch_data():
 
     print("Downloading launch data...")
 
-    response = requests.get(SPACEX_API)
-
-    response.raise_for_status()
-
-    launches = response.json()
+    data = get_launch_library_data()
 
     RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-    output_file = RAW_DATA_DIR / "spacex_launches.json"
+    output_file = RAW_DATA_DIR / "launch_library.json"
 
     with open(output_file, "w", encoding="utf-8") as file:
-        json.dump(launches, file, indent=4)
+        json.dump(data, file, indent=4)
 
-    print(f"Saved {len(launches)} launches.")
-    print(f"Location: {output_file}")
+    print("Download complete.")
+    print(f"Saved to {output_file}")
 
 
 if __name__ == "__main__":
-    fetch_launches()
+    save_raw_launch_data()
