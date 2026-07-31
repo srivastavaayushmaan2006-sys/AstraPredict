bb3232ad1e351e53f9d5bd0c58caa73bc2345788
Ayushmaan Srivastava <srivastavaayushmaan2006@gmail.com>
2026-07-31 00:27:50 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,4 +4,6 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
 
-SPACEX_API = "https://api.spacexdata.com/v5/launches"
+API_ENDPOINTS = {
+    "launch_library": "https://ll.thespacedevs.com/2.2.0/launch/?limit=100"
+}
